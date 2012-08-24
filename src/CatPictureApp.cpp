2500f202ea92a4a86d5987f91bc99a063e2401d4
harmonbc <harmonbc@muohio.edu>
2012-08-24 19:14:16 -0400

    Added Comments

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file CatPictureApp.cpp
+ * A simple app that changes the color of the background
+ *
+ * @author Brandon Harmon
+ * @date 2012-08-24
+ *
+ * @note This file is (c) 2012. It is licensed under the 
+ * CC BY 3.0 license (http://creativecommons.org/licenses/by/3.0/),
+ * which means you are free to use, share, and remix it as long as you
+ * give attribution. Commercial uses are allowed.
+ */
+
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 
@@ -31,6 +44,10 @@ void CatPictureApp::mouseDown( MouseEvent event )
 void CatPictureApp::update()
 {
 
+/// 0.0f is a flag that the color is out of range. After a color has
+/// finished its display it will be set to 0 and the next range of RGB
+/// will be activated.
+
 	if(red != 0.0f)
 		red = red+ 0.01f;
 
@@ -57,6 +74,7 @@ void CatPictureApp::update()
 void CatPictureApp::draw()
 {
 	gl::clear( Color( red, green, blue ) );
+
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
